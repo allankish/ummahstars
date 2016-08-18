903877c93164fd6c596c8684833b961b7ece4202
colan <thiru@colanonline.com>
2016-08-18 19:26:38 +0530

    DB Config file updated

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',//208.91.198.110
-	'username' => 'root',//colandbe_devusr
-	'password' => 'colan123',//Colan#20o4
-	'database' => 'ummahstars',//colandbe_ummahstars
+	'hostname' => '208.91.198.110',
+	'username' => 'colandbe_devusr',
+	'password' => 'Colan#20o4',
+	'database' => 'colandbe_ummahstars',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
