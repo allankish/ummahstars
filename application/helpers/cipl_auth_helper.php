73f8760a7a85685ff0c45db19135d231d050615a
colan <thiru@colanonline.com>
2016-08-31 14:57:03 +0530

    admin user model and auth helper updated.

--- application/helpers/cipl_auth_helper.php
+++ application/helpers/cipl_auth_helper.php
@@ -1,16 +1,32 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-if ( ! function_exists('cipl_admin_auth'))
-{
-    function cipl_admin_auth()
-    {
-		$CI =& get_instance();
-		
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+if (!function_exists('cipl_admin_auth')) {
+
+    function cipl_admin_auth() {
+        $CI = & get_instance();
+
         $is_admin_auth = $CI->session->userdata('admin_auth');
-        if(!isset($is_admin_auth) || $is_admin_auth != true)
-        {
-			redirect('/usadmin/login');
+        if (!isset($is_admin_auth) || $is_admin_auth != true) {
+            redirect('/usadmin/login');
             die();
         }
     }
+
+}
+
+if (!function_exists('cipl_user_auth')) {
+
+    function cipl_user_auth() {
+        $CI = & get_instance();
+
+        $is_user_auth = $CI->session->userdata('user_auth');
+        if (!isset($is_user_auth) || $is_user_auth != true) {
+            redirect('/login');
+            die();
+        }
+    }
+
 }
